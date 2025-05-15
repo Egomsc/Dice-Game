610b7f0aa44404992be58f86a0e3bc8165b5104e
Santus Onuegbu <benbenedicta56@gmail.com>
2025-05-15 08:46:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1; // 1-6 randoms
 var randomDice1 = ("dice"+randomNumber1+".png");
 var randomImage1 = ("images/"+randomDice1);
-document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
+document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
 
 
 // player 2 codes
 var randomNumber2 = Math.floor(Math.random()*6)+1; 
 var randomDice2 = ("dice"+randomNumber2+".png");
 var randomImage2 = ("images/"+randomDice2);
-document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
 
 // the scoring patter
 if(randomNumber1 > randomNumber2){
